23da353d1cd2b486849dc9e8f0ef7f2a16c4917e
Collard <jean-gaspar.collard@isen-lille.fr>
2017-01-09 16:34:14 +0100

    add: ajout test implementation and addcell test

--- jee-bootstrap-core/src/test/java/org/isen/bootstrap/core/DameGameTest.java
+++ jee-bootstrap-core/src/test/java/org/isen/bootstrap/core/DameGameTest.java
@@ -7,10 +7,20 @@ public class DameGameTest {
 
     Private DameBoard board;
 
+    @Before
+    public void testImplementation(){
+        board = new boardImpl();
+    }
+
     @Test
     public void can_instantiate_board() throws Exception {
         assertThat(board).isNull();
     }
 
-    
+    @test
+    public void can_add_cell(){
+        Cell celltest = board.addCell();
+        assertThat(celltest).isNull();
+    }
+
 }
