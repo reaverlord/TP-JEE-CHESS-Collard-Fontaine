1590315d798080130078d657f0b7f486e25fdf11
Collard <jean-gaspar.collard@isen-lille.fr>
2017-01-09 16:24:18 +0100

    add : ajout test board exist

--- jee-bootstrap-core/src/main/java/isen/jee/bootstrap/core/ChipColour.java
+++ jee-bootstrap-core/src/main/java/isen/jee/bootstrap/core/ChipColour.java
@@ -1,8 +1,5 @@
 package isen.jee.bootstrap.core;
 
-/**
- * Created by dmetzler on 03/09/2014.
- */
 public enum ChipColour {
     WHRITE, BLACK
 }
--- jee-bootstrap-core/src/test/java/org/isen/bootstrap/core/DameGameTest.java
+++ jee-bootstrap-core/src/test/java/org/isen/bootstrap/core/DameGameTest.java
@@ -4,4 +4,13 @@ package test.java.org.isen.bootstrap.core;
  * Created by Romain on 09/01/2017.
  */
 public class DameGameTest {
+
+    Private DameBoard board;
+
+    @Test
+    public void can_instantiate_board() throws Exception {
+        assertThat(board).isNull();
+    }
+
+    
 }
