ca1d262bcb0f76ea6c0d28c977fa656f7fbe8fa3
Collard <jean-gaspar.collard@isen-lille.fr>
2017-01-09 16:49:24 +0100

    ajout Dameboard class

---
+++ jee-bootstrap-core/src/main/java/isen/jee/bootstrap/core/DameBoard.java
@@ -0,0 +1,7 @@
+package isen.jee.bootstrap.core;
+
+/**
+ * Created by Collard on 09/01/2017.
+ */
+public class DameBoard {
+}
--- jee-bootstrap-core/src/test/java/org/isen/bootstrap/core/DameGameTest.java
+++ jee-bootstrap-core/src/test/java/org/isen/bootstrap/core/DameGameTest.java
@@ -3,9 +3,10 @@ package test.java.org.isen.bootstrap.core;
 /**
  * Created by Romain on 09/01/2017.
  */
+
 public class DameGameTest {
 
-    Private DameBoard board;
+    private DameBoard board;
 
     @Before
     public void testImplementation(){
@@ -17,10 +18,14 @@ public class DameGameTest {
         assertThat(board).isNull();
     }
 
-    @test
-    public void can_add_cell(){
+    @Test
+    public void can_add_cell() throws Exception {
         Cell celltest = board.addCell();
         assertThat(celltest).isNull();
     }
 
+    @Test
+    public void cell_has color() throws Exception {
+        assertThat(celltest.color).isNull();
+    }
 }
