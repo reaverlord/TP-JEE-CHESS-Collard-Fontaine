afae57d6d7ab575d3f44e716d9a2831f7d88a89d
Collard <jean-gaspar.collard@isen-lille.fr>
2017-01-16 14:08:07 +0100

    add initboard()

--- jee-bootstrap-core/src/main/java/org/isen/bootstrap/core/DameBoard.java
+++ jee-bootstrap-core/src/main/java/org/isen/bootstrap/core/DameBoard.java
@@ -1,7 +1,26 @@
 package org.isen.bootstrap.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Romain on 09/01/2017.
  */
 public class DameBoard {
+    public static final int caseNumber= 9;
+    public int numberOfChip = 1;
+
+    List<List<ChipColour>> board = new ArrayList<>(caseNumber);
+
+    public void DameGameImpl(){
+        initBoard();
+    }
+
+    private void initBoard() {
+        for (int i = 0; i < caseNumber; i++){
+            board.add(new ArrayList<ChipColour>(caseNumber));
+        }
+    }
+
+
 }
