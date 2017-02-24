7dd2dbbda146fcc6b14dbab9585febfb500aca26
Romain Fontaine <romain.fontaine@isen-lille.fr>
2017-02-24 11:06:16 +0100

    finalisation def dameBaord

--- jee-bootstrap-core/src/main/java/org/isen/bootstrap/core/DameBoard.java
+++ jee-bootstrap-core/src/main/java/org/isen/bootstrap/core/DameBoard.java
@@ -72,6 +72,81 @@ public class DameBoard {
         getCell(row, col).setChip(chip);
     }
 
+    public Chip.ChipColor getCurrentTurn() {
+        if (this.turns.size() % 2 == 0) {
+            return Chip.ChipColor.WHITE;
+        } else {
+            return Chip.ChipColor.BLACK;
+        }
+    }
+
+    public void playTurn(int srcRow, int srcCol, int dstRow, int dstCol) throws IllegalMoveException {
+        if (dstRow % 2 == 1) {
+            if (dstCol % 2 == 0) {
+                throw new IllegalMoveException();
+            }
+        } else {
+            if (dstCol % 2 == 1) {
+                throw new IllegalMoveException();
+            }
+        }
+
+        try {
+            Chip chip = getChip(srcRow, srcCol);
+            if (chip.getChipColor() == Chip.ChipColor.WHITE) {
+                if (dstRow <= srcRow && chip.getChipType() == Chip.ChipType.CHIP) {
+                    throw new IllegalMoveException();
+                }
+            } else {
+                if (dstRow >= srcRow && chip.getChipType() == Chip.ChipType.CHIP) {
+                    throw new IllegalMoveException();
+                }
+            }
+            if (hasChip(dstRow, dstCol)) {
+                throw new IllegalMoveException();
+            }
+            this.turns.add(new Turn(srcRow, srcCol, dstRow, dstCol));
+            setChip(dstRow, dstCol, chip);
+            promoteToQueens();
+        } catch (BoardBoundsException e) {
+            throw new IllegalMoveException();
+        } catch (CellEmptyException e) {
+            throw new IllegalMoveException();
+        }
+    }
+
+    private void promoteToQueens() throws CellEmptyException, BoardBoundsException {
+        for (int col=1; col< COLS; col++) {
+            if (hasChip(ROWS, col)) {
+                if (getChip(ROWS, col).getChipType() == Chip.ChipType.CHIP) {
+                    getChip(ROWS, col).setChipType(Chip.ChipType.QUEEN);
+                }
+            }
+
+            if (hasChip(1, col)) {
+                if (getChip(1, col).getChipType() == Chip.ChipType.CHIP) {
+                    getChip(1, col).setChipType(Chip.ChipType.QUEEN);
+                }
+            }
+        }
+    }
+
+    public int chipCount(Chip.ChipColor color) {
+        int result = 0;
+        for (int row=1; row <= ROWS; row++) {
+            for (int col=1; col <= COLS; col++) {
+                    if (hasChip(row, col)) {
+                        if (getChip(row, col).getchipColor() == color) {
+                            result++;
+                        }
+                    }
+
+            }
+        }
+
+        return result;
+    }
+
 
 
 
