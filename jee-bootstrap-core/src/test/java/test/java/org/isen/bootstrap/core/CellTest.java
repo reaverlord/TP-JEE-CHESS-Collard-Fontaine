7e495f28c13ef474793a05f110ee71c528f7b7b7
Collard <jean-gaspar.collard@isen-lille.fr>
2017-02-24 12:01:09 +0100

    add : cell and chip tests

--- jee-bootstrap-core/src/test/java/test/java/org/isen/bootstrap/core/CellTest.java
+++ jee-bootstrap-core/src/test/java/test/java/org/isen/bootstrap/core/CellTest.java
@@ -1,7 +1,66 @@
 package test.java.org.isen.bootstrap.core;
 
+import org.isen.bootstrap.core.Cell;
+import org.isen.bootstrap.core.Chip;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Created by Romain on 24/02/2017.
  */
 public class CellTest {
+
+    private Cell testCell;
+    private Chip testChip;
+
+
+
+    @Before
+    public void init(){
+        testChip = new Chip(Chip.ChipColor.WHITE, Chip.ChipType.CHIP);
+        testCell = new Cell(this.testChip,0,0);
+    }
+
+    @Test
+    public void testHasChip(){
+        assertTrue(this.testCell.hasChip());
+    }
+
+    @Test
+    public void testSetChip(){
+        this.testCell = new Cell(0,0);
+        this.testCell.setChip(this.testChip);
+        assertTrue(this.testCell.hasChip());
+    }
+
+    @Test
+    public void testGetChip(){
+        this.testCell.setChip(this.testChip);
+        assertNotEquals(this.testCell.getChip(),null);
+    }
+
+    @Test
+    public void testSetRow(){
+        this.testCell.setRow(2);
+        assertEquals(this.testCell.getRow(),2);
+    }
+
+    @Test
+    public void testSetCol(){
+        this.testCell.setCol(5);
+        assertEquals(this.testCell.getCol(),5);
+    }
+
+    @Test
+    public void testGetRow(){
+        this.testCell = new Cell(0,0);
+        assertEquals(this.testCell.getRow(),0);
+    }
+
+    @Test
+    public void testGetCol(){
+        this.testCell = new Cell(0,0);
+        assertEquals(this.testCell.getCol(),0);
+    }
 }
--- jee-bootstrap-core/src/test/java/test/java/org/isen/bootstrap/core/ChipTest.java
+++ jee-bootstrap-core/src/test/java/test/java/org/isen/bootstrap/core/ChipTest.java
@@ -1,7 +1,65 @@
 package test.java.org.isen.bootstrap.core;
 
+import org.isen.bootstrap.core.Chip;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Created by Romain on 24/02/2017.
  */
 public class ChipTest {
+
+    private Chip chipTestOne;
+    private Chip chipTestTwo;
+    private Chip chipTestThree;
+    private Chip chipTestFour;
+
+    @Before
+    public void init() throws Exception{
+        chipTestOne = new Chip(Chip.ChipColor.BLACK, Chip.ChipType.CHIP);
+        chipTestTwo = new Chip(Chip.ChipColor.WHITE, Chip.ChipType.CHIP);
+        chipTestThree = new Chip(Chip.ChipColor.BLACK, Chip.ChipType.QUEEN);
+        chipTestFour = new Chip(Chip.ChipColor.WHITE, Chip.ChipType.QUEEN);
+    }
+
+    @Test
+    public void testGetChipColour() throws Exception{
+        assertEquals(this.chipTestOne.getChipColor(), Chip.ChipColor.BLACK);
+        assertEquals(this.chipTestFour.getChipColor(), Chip.ChipColor.WHITE);
+        assertEquals(this.chipTestTwo.getChipColor(), Chip.ChipColor.WHITE);
+        assertEquals(this.chipTestThree.getChipColor(), Chip.ChipColor.BLACK);
+    }
+
+    @Test
+    public void testGetChipType() throws Exception{
+        assertEquals(this.chipTestOne.getChipType(), Chip.ChipType.CHIP);
+        assertEquals(this.chipTestTwo.getChipType(), Chip.ChipType.CHIP);
+        assertEquals(this.chipTestThree.getChipType(), Chip.ChipType.QUEEN);
+        assertEquals(this.chipTestFour.getChipType(), Chip.ChipType.QUEEN);
+    }
+
+    @Test
+    public void testSetChipColour(){
+        this.chipTestOne.setChipColor(Chip.ChipColor.WHITE);
+        assertEquals(this.chipTestOne.getChipColor(), Chip.ChipColor.WHITE);
+        this.chipTestTwo.setChipColor(Chip.ChipColor.BLACK);
+        assertEquals(this.chipTestTwo.getChipColor(), Chip.ChipColor.BLACK);
+        this.chipTestThree.setChipColor(Chip.ChipColor.WHITE);
+        assertEquals(this.chipTestThree.getChipColor(), Chip.ChipColor.WHITE);
+        this.chipTestFour.setChipColor(Chip.ChipColor.BLACK);
+        assertEquals(this.chipTestFour.getChipColor(), Chip.ChipColor.BLACK);
+    }
+
+    @Test
+    public void testSetChipType(){
+        this.chipTestOne.setChipType(Chip.ChipType.QUEEN);
+        assertEquals(this.chipTestOne.getChipType(), Chip.ChipType.QUEEN);
+        this.chipTestTwo.setChipType(Chip.ChipType.QUEEN);
+        assertEquals(this.chipTestTwo.getChipType(), Chip.ChipType.QUEEN);
+        this.chipTestThree.setChipType(Chip.ChipType.CHIP);
+        assertEquals(this.chipTestThree.getChipType(), Chip.ChipType.CHIP);
+        this.chipTestFour.setChipType(Chip.ChipType.CHIP);
+        assertEquals(this.chipTestFour.getChipType(), Chip.ChipType.CHIP);
+    }
 }
